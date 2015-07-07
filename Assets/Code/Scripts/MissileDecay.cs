73b9d5871d4e257e92a09586f535cc9ad34f70f1
treyzania <treyzania@gmail.com>
2015-07-06 22:43:41 -0400

    Added a max range to missiles.

---
+++ Assets/Code/Scripts/MissileDecay.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MissileDecay : MonoBehaviour {
+
+	public float MaxRange = 10F;
+	private Vector3 StartPos = Vector3.zero;
+	
+	// Use this for initialization
+	void Start () {
+		this.StartPos = this.transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		if ((this.transform.position - this.StartPos).sqrMagnitude >= Mathf.Pow(MaxRange, 2)) {
+			GameObject.Destroy(this.gameObject);
+		}
+		
+	}
+}
