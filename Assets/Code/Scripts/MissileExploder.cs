d1418f486e7258d571df57f421447853b04972fa
treyzania <treyzania@gmail.com>
2015-08-18 18:45:12 -0400

    Added missile damage application script.

---
+++ Assets/Code/Scripts/MissileExploder.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MissileExploder : MonoBehaviour {
+
+	public float BaseDamage = 0F;
+	
+	void OnColllisionEnter(Collision col) {
+		
+		HealthManager hm = col.gameObject.GetComponent<HealthManager>();
+		
+		if (hm != null) {
+			
+			hm.DealDamage(BaseDamage);
+			
+		}
+		
+	}
+	
+}
