911061c094c2ac57728404f9e836525cee14a019
treyzania <treyzania@gmail.com>
2015-08-22 00:15:06 -0400

    Player's health meter won't flip around when moving anymore.

---
+++ Assets/Code/Scripts/KeepAbsoluteOrientation.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class KeepAbsoluteOrientation : MonoBehaviour {
+
+	private Quaternion initRot;
+	
+	void Start() {
+		this.initRot = this.transform.rotation;
+	}
+	
+	void FixedUpdate() {
+		this.transform.rotation = this.initRot;
+	}
+	
+}
