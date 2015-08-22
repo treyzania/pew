fd480ee0f1b7fa9670d834b052e75cc9e5c1e7c4
treyzania <treyzania@gmail.com>
2015-08-22 16:40:15 -0400

    Missiles do damage now and enemies can die from them.

--- Assets/Code/Scripts/MissileExploder.cs
+++ Assets/Code/Scripts/MissileExploder.cs
@@ -5,12 +5,15 @@ public class MissileExploder : MonoBehaviour {
 
 	public float BaseDamage = 0F;
 	
-	void OnColllisionEnter(Collision col) {
+	void OnTriggerEnter(Collider col) {
 		
 		HealthManager hm = col.gameObject.GetComponent<HealthManager>();
 		
+		Debug.Log("I hit something!");
+		
 		if (hm != null) {
 			
+			Debug.Log("Dealing damage!");
 			hm.DealDamage(BaseDamage);
 			
 		}
