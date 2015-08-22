7bc1dcaa9f61893bfabda3198197cf0a5d28fd4e
treyzania <treyzania@gmail.com>
2015-08-22 00:12:32 -0400

    Added a turn towards travel direction script.

---
+++ Assets/Code/Scripts/TurnTowardsTravel.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TurnTowardsTravel : MonoBehaviour {
+
+	public float RotationSpeed = 1F;
+	
+	private Rigidbody rb;
+	
+	void Start() {
+		this.rb = this.GetComponent<Rigidbody>();
+	}
+	
+	void Update() {
+		
+		Quaternion currentRot = this.transform.rotation;
+		Quaternion travelRot = Quaternion.LookRotation(this.rb.velocity * -1);
+		
+		this.transform.rotation = Quaternion.Slerp(currentRot, travelRot, RotationSpeed * Time.deltaTime);
+		
+	}
+	
+}
