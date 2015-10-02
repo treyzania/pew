d0e3394442c74262aeac602d89a7eb726712d06d
treyzania <treyzania@gmail.com>
2015-10-01 21:55:56 -0400

    HealthMeter now automatically links with a HealthManager, if present on the object.

--- Assets/Code/Scripts/Player/HealthMeter.cs
+++ Assets/Code/Scripts/Player/HealthMeter.cs
@@ -9,7 +9,22 @@ public class HealthMeter : MonoBehaviour {
 	public float MaxHealth = 100F;
 	public float CurrentHealth = 100F;
 	
+	private HealthManager hm;
+	
+	void Start() {
+		
+		this.hm = this.GetComponent<HealthManager>();
+		
+	}
+	
 	void Update () {
+		
+		if (this.hm != null) {
+			
+			this.MaxHealth = hm.MaxHealth;
+			this.CurrentHealth = hm.Health;
+			
+		}
 		
 		this.ImageComponent.fillAmount = Mathf.Clamp01(this.CurrentHealth / this.MaxHealth);
 		
