209c6df3e03056ac10574113941d0ebb26133360
treyzania <treyzania@gmail.com>
2015-10-13 22:43:18 -0400

    Started work on Menu and Death scenes.

---
+++ Assets/Code/Scripts/SetScene.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SetScene : MonoBehaviour {
+	
+	public string LevelName;
+	
+	public void DoChange() {
+		
+		Application.LoadLevel(this.LevelName);
+		
+	}
+	
+}
