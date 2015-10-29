46a6cd1987c827b8343c21c30037c824f52e2c39
treyzania <treyzania@gmail.com>
2015-10-29 00:55:43 -0400

    More hacks-related stuff.

--- Assets/Code/Scripts/MainGame/AddMoney.cs
+++ Assets/Code/Scripts/MainGame/AddMoney.cs
@@ -1,1 +1,29 @@
+using UnityEngine;
+using Pew.Player;
 
+public class AddMoney : MonoBehaviour {
+	
+	public int DollaDollaBills = 0;
+	
+	public void OnButton() {
+		
+		StoredPlayerData.PLAYER_DATA.Money += this.DollaDollaBills;
+		StoredPlayerData.PLAYER_DATA.Save();
+		
+	}
+	
+	public void ResetMoney() {
+		
+		StoredPlayerData.PLAYER_DATA.Money = 0;
+		StoredPlayerData.PLAYER_DATA.Save();
+		
+	}
+	
+	public void ResetEverything() {
+		
+		StoredPlayerData.PLAYER_DATA = new StoredPlayerData();
+		StoredPlayerData.PLAYER_DATA.Save();
+		
+	}
+	
+}
