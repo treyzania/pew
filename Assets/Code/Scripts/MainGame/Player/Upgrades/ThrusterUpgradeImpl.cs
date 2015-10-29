19d552cec4ea42f5d8d83b479f12fab098c1ea6a
treyzania <treyzania@gmail.com>
2015-10-28 23:09:57 -0400

    Thruster upgrades implementation.

---
+++ Assets/Code/Scripts/MainGame/Player/Upgrades/ThrusterUpgradeImpl.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+using Pew.Player;
+
+public class ThrusterUpgradeImpl : UpgradeImplBase {
+
+	public ThrusterUpgradeEntry[] ThrusterValues;
+	
+	void Start () {
+		
+		this.DoMaterialChange();
+		
+		// Update the motion values.
+		GyroMove gm = this.GetComponent<GyroMove>();
+		
+		gm.VelocityFactor = this.ThrusterValues[this.TrackIndex].VelocityFactor;
+		gm.MaxVelocity = this.ThrusterValues[this.TrackIndex].MaxVelocity;
+		
+		// Disable it to speed things up.
+		this.enabled = false;
+		
+	}
+	
+	[System.Serializable]
+	public class ThrusterUpgradeEntry {
+		
+		[Range(0, 5)] public float VelocityFactor;
+		[Range(0, 50)] public float MaxVelocity;
+		
+	}
+	
+}
