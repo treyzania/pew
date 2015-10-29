d52b38d2ba3b19921dbef0f9c80ecf2dc5f33583
treyzania <treyzania@gmail.com>
2015-10-28 20:16:45 -0400

    Fixed a small but major issue with the UpgradeTrack ScriptableObject type.

--- Assets/Code/Player/Upgrades.cs
+++ Assets/Code/Player/Upgrades.cs
@@ -25,13 +25,4 @@ namespace Pew.Player {
 		
 	}
 	
-	[System.Serializable]
-	public class UpgradeTrack : ScriptableObject {
-		
-		public string Name;
-		public ShipPart Part;
-		public UpgradeEntry[] Entries;
-		
-	}
-	
 }
