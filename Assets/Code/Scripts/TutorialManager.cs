55f4b522a116f80641863f0b0577f86327b345b1
treyzania <treyzania@gmail.com>
2015-12-03 21:22:06 -0500

    Made a brief tutorial, giving the player an idea of what's going on and how to move and shoot.

---
+++ Assets/Code/Scripts/TutorialManager.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TutorialManager : MonoBehaviour {
+
+	private EnemySpawner spawner;
+	private GyroMove playerMotion;
+	
+	void Start () {
+		
+		this.spawner = GameObject.Find("EnemySpawner").GetComponent<EnemySpawner>();
+		this.playerMotion = GameObject.Find("Player").GetComponent<GyroMove>();
+		
+		if (PlayerPrefs.GetInt("done_tutorial") != 1) {
+			
+			spawner.enabled = false;
+			playerMotion.enabled = false;
+			
+		} else {
+			this.Die();
+		}
+		
+	}
+	
+	public void Die() {
+		
+		Debug.Log("Tutorial closed.");
+		
+		PlayerPrefs.SetInt("done_tutorial", 1);
+		PlayerPrefs.Save();
+		
+		spawner.enabled = true;
+		playerMotion.enabled = true;
+		
+		GameObject.Destroy(this.gameObject);
+		
+		Time.timeScale = 1F;
+		
+	}
+	
+}
